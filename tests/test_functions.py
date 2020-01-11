3c8e3d411a80db225fe01315e9390196e527cd14
Alpha <alphagma@gmail.com>
2020-01-11 18:15:35 -0500

    Testing structure

---
+++ tests/test_functions.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from text_generator import functions
+
+import unittest
+
+class LowerBoundTestSuite(unittest.TestCase):
+    """lower_bound test cases."""
+
+    def test_framework_test(self):
+        assert True
+
+if __name__ == 'main':
+    unittest.main()
